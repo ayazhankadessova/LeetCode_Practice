c5dfaa9fc3da4da2b868ec77bae13dd318d38d73
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-02-18 15:02:08 +0800

    fix: format

--- DSA_Book/Chapter3Exercises/src/SLL_headeronly_sorted.java
+++ DSA_Book/Chapter3Exercises/src/SLL_headeronly_sorted.java
@@ -1,5 +1,3 @@
-import java.util.Iterator;
-
 /*
  * Assume that a singly linked list is implemented with a header node, but no tail node, and that it maintains only a reference to the header node. Write a class that includes methods to
 a. return the size of the linked list
@@ -14,13 +12,13 @@ Approach:
 2. Initialize head.next
 3. Contains: iter with while and checl
 4. add: check if contains, if not, create p 
-5. To keep sorted , check if it is more than header.next. 
+    * **To keep sorted** , check if it is more than header.next. 
     * If yes, insert as usual: p.next = header.next; header.next = p;
     * If no, start iterating from header to end, while remembering the previous node until we find a node which is more than one we want to insert.
     * Make new node point to where previous node pointed.
     * Make previous node point to new node.
 
-5. remove: check if contains. Do tracing node and current node. When found node to remove -> prev.next = current.next
+6. remove: check if contains. Do tracing node and current node. When found node to remove -> prev.next = current.next
 
  */
 
@@ -28,69 +26,70 @@ public class SLL_headeronly_sorted<T extends Comparable<T>> {
 
     private Node header;
     int size;
-    
+
     SLL_headeronly_sorted() {
         header = new Node(null);
         size = 0;
         header.next = null;
     }
 
-
     class Node {
         T data;
         Node next;
+
         Node(T data) {
             this.data = data;
         }
     }
-   
+
     public int getSize() {
         return size;
     }
-    
+
     public void printList() {
         Node current;
-        if (header.next!= null) {
+        if (header.next != null) {
             current = header.next;
         } else {
             return;
         }
-        
-        while (current!= null) {
+
+        while (current != null) {
             System.out.print(current.data + " ");
             current = current.next;
         }
         System.out.println();
     }
 
-
-    boolean contains( T data ){
-    Node p = header.next;
-    while (p != null) {
-      if (data.equals(p.data)) {
-        return true;
-      }      else {
-        p = p.next;
-      }
-    }
-    return false;
-    }
-
-    boolean add( T data ) {
-    if (contains(data)) {
+    boolean contains(T data) {
+        Node p = header.next;
+        while (p != null) {
+            if (data.equals(p.data)) {
+                return true;
+            } else {
+                p = p.next;
+            }
+        }
         return false;
     }
-      else { 
-      Node p = new Node(data);
-        // if (header.next != null && p.data.compareTo(header.next.data) < 0) {
-        //      p.next = header.next;
-        //      header.next = p;
-        // } else {
+
+    /*
+     * Add preserving ascending order
+     */
+    boolean add(T data) {
+        if (contains(data)) {
+            return false;
+        } else {
+            Node p = new Node(data);
+            // if (header.next != null && p.data.compareTo(header.next.data) < 0) {
+            // p.next = header.next;
+            // header.next = p;
+            // } else {
             Node previous = header;
             Node current = header.next;
 
-            while (current != null && current.data.compareTo(data) < 0 ) {
-                System.out.println(current.data + " is less than " + data );
+            while (current != null && current.data.compareTo(data) < 0) {
+                System.out.println(current.data + " is less than " + data);
                 previous = current;
                 current = current.next;
             }
@@ -99,14 +98,14 @@ public class SLL_headeronly_sorted<T extends Comparable<T>> {
         }
         // }
 
-      size++;
-      return true;
+        size++;
+        return true;
     }
 
-
-  /*
-   * Set a node which will trace the previous elem and when we find an item to delete make the tracer point to deleted's next node.
-   */
+    /*
+     * Set a node which will trace the previous elem and when we find an item to
+     * delete make the tracer point to deleted's next node.
+     */
     public void remove(T data) {
         if (!contains(data)) {
             System.out.println("Data not found");
@@ -126,18 +125,16 @@ public class SLL_headeronly_sorted<T extends Comparable<T>> {
             size--;
 
         }
-        
-
     }
 
     public static void main(String[] args) {
 
         SLL_headeronly_sorted<Integer> iterList = new SLL_headeronly_sorted<Integer>();
 
-        int[] arrayInt = {1, 3, 4, 6, 3, 8, 25};
+        int[] arrayInt = { 1, 3, 4, 6, 3, 8, 25 };
 
         // Add elements keeping the ascending order
-        for (int elem: arrayInt) {
+        for (int elem : arrayInt) {
             iterList.add(elem);
         }
 
@@ -150,6 +147,6 @@ public class SLL_headeronly_sorted<T extends Comparable<T>> {
         System.out.println("Get the size of the list: " + iterList.size);
         System.out.print("Try to remove 10, which is NOT in the list: ");
         iterList.remove(10);
-        
+
     }
 }
