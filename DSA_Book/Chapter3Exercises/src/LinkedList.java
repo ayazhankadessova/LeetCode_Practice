1dc01c382012065b4553c72812f52a8e4ed80c39
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-02-20 10:08:30 +0800

    add: LazyDeletion

--- DSA_Book/Chapter3Exercises/src/LinkedList.java
+++ DSA_Book/Chapter3Exercises/src/LinkedList.java
@@ -1,5 +1,4 @@
 
-
 /*
 
 3.2 Swap two given nodes of a linked list
@@ -16,20 +15,21 @@
 class Node {
     int data;
     Node next;
-    Node(int d)
-    {
+
+    Node(int d) {
         data = d;
         next = null;
     }
 }
- 
+
 class LinkedList {
     Node head; // head of list
- 
-    /* Function to swap Nodes x and y in linked list by
-       changing links */
-    public void swapNodes(int x, int y)
-    {
+
+    /*
+     * Function to swap Nodes x and y in linked list by
+     * changing links
+     */
+    public void swapNodes(int x, int y) {
         // 1. if x and y are the same -> no change
         if (x == y)
             return;
@@ -37,7 +37,7 @@ class LinkedList {
         // 2. Search for X, keep track of prevX and currX
         Node prevX = null;
         Node currentX = head;
-        while (currentX!=null && currentX.data!=x) {
+        while (currentX != null && currentX.data != x) {
             prevX = currentX;
             currentX = currentX.next;
         }
@@ -45,71 +45,71 @@ class LinkedList {
         // 3. Search for Y, keep track of prevY and currY
         Node prevY = null;
         Node currentY = head;
-        while (currentY!=null && currentY.data!=y) {
+        while (currentY != null && currentY.data != y) {
             prevY = currentY;
             currentY = currentY.next;
         }
 
         // 4. Check if either x or y don't exist
 
-        if (currentX==null || currentY==null) {
+        if (currentX == null || currentY == null) {
             return;
         }
 
         // 5. If x not head -> prevX.next = currY. If head -> Y
-        if (prevX!=null) {
+        if (prevX != null) {
             prevX.next = currentY;
         } else {
             head = currentY;
         }
 
         // 6. Same for y
-        if (prevY!=null) {
+        if (prevY != null) {
             prevY.next = currentX;
         } else {
             head = currentX;
         }
 
-        // 7. Swap next pointers 
+        // 7. Swap next pointers
 
         Node temp = currentX.next;
         currentX.next = currentY.next;
         currentY.next = temp;
 
-
     }
- 
+
     /* Function to add Node at beginning of list. */
-    public void push(int new_data)
-    {
+    public void push(int new_data) {
         /* 1. alloc the Node and put the data */
         Node new_Node = new Node(new_data);
- 
+
         /* 2. Make next of new Node as head */
         new_Node.next = head;
- 
+
         /* 3. Move the head to point to new Node */
         head = new_Node;
     }
- 
-    /* This function prints contents of linked list starting
-       from the given Node */
-    public void printList()
-    {
+
+    /*
+     * This function prints contents of linked list starting
+     * from the given Node
+     */
+    public void printList() {
         Node tNode = head;
         while (tNode != null) {
             System.out.print(tNode.data + " ");
             tNode = tNode.next;
         }
     }
- 
+
     /* Driver program to test above function */
-    public static void main(String[] args)
-    {
-        LinkedList llist = new LinkedList();
- 
-        /* The constructed linked list is:
-            1->2->3->4->5->6->7 */
+    public static void main(String[] args) {
+        LinkedList_LazyDeletion llist = new LinkedList_LazyDeletion();
+
+        /*
+         * The constructed linked list is:
+         * 1->2->3->4->5->6->7
+         */
         llist.push(7);
         llist.push(6);
         llist.push(5);
@@ -117,16 +117,15 @@ class LinkedList {
         llist.push(3);
         llist.push(2);
         llist.push(1);
- 
+
         System.out.print(
-            "\n Linked list before calling swapNodes() ");
+                "\n Linked list before calling swapNodes() ");
         llist.printList();
- 
+
         llist.swapNodes(4, 3);
- 
+
         System.out.print(
-            "\n Linked list after calling swapNodes() ");
+                "\n Linked list after calling swapNodes() ");
         llist.printList();
     }
 }
-    
---
+++ DSA_Book/Chapter3Exercises/src/LinkedList_LazyDeletion.java
@@ -0,0 +1,144 @@
+
+/*
+
+3.2 Lazy Deletion
+
+*/
+class Node<AnyType> {
+    AnyType data;
+    Node next;
+    Boolean Marked;
+    // set to 1 if deleted, and 0 otherwise
+
+    Node(AnyType d) {
+        data = d;
+        next = null;
+        Marked = false;
+    }
+}
+
+class LinkedList_LazyDeletion<AnyType> {
+    Node head; // head of list
+
+    int Total_Count = 0; // the total number of elements in the list
+    int Deleted_Count = 0; // the total number of deleted elements
+
+    /* Function to add Node at beginning of list. */
+    public void push(int new_data) {
+        /* 1. alloc the Node and put the data */
+        Node new_Node = new Node(new_data);
+
+        /* 2. Make next of new Node as head */
+        new_Node.next = head;
+
+        /* 3. Move the head to point to new Node */
+        head = new_Node;
+    }
+
+    Node Find(AnyType X) {
+        Node P = this.head;
+        while (P != null && (P.data != X || P.Marked == true))
+            P = P.next;
+        return P;
+    }
+
+    void Lazy_Deletion(AnyType X) {
+        Node P;
+        P = Find(X);
+        System.out.println("Deleted: " + P.data);
+        if (P != null) {
+            P.Marked = true;
+            Deleted_Count++;
+            if (Total_Count - Deleted_Count == Deleted_Count)
+                // the number of deleted elements is the same as the number
+                // of nondeleted elements in the list
+                Deletion();
+        }
+    }
+
+    boolean IsLast(Node P) {
+        return P.next == null;
+    }
+
+    void Deletion() {
+        Node P, Previous, TmpCell;
+        if (head.Marked == true) {
+            this.head = head.next;
+        }
+
+        Previous = this.head;
+        P = head.next;
+
+        while (!IsLast(P)) {
+            System.out.println("Prev:" + Previous.data);
+            System.out.println("P:" + P.data);
+
+            if (P.Marked) {
+                TmpCell = P;
+                P = P.next;
+                Previous.next = P;
+                // free(TmpCell);
+                Total_Count--;
+                Deleted_Count--;
+            } else {
+                Previous = P;
+                P = P.next;
+            }
+        }
+
+        // if last item deleted
+        if (P.next == null && P.Marked == true) {
+            // System.out.println(Previous);
+            Previous.next = null;
+        }
+    }
+
+    /*
+     * This function prints contents of linked list starting
+     * from the given Node
+     */
+    public void printList() {
+        Node tNode = head;
+        while (tNode != null) {
+            // if (tNode.Marked != true) {
+            System.out.print(tNode.data + " ");
+            // }
+            tNode = tNode.next;
+        }
+    }
+
+    /* Driver program to test above function */
+    public static void main(String[] args) {
+        LinkedList_LazyDeletion llist = new LinkedList_LazyDeletion();
+
+        /*
+         * The constructed linked list is:
+         * 1->2->3->4->5->6->7
+         */
+        llist.push(7);
+        llist.push(6);
+        llist.push(5);
+        llist.push(4);
+        llist.push(3);
+        llist.push(2);
+        llist.push(1);
+
+        System.out.print(
+                "\n Linked list");
+        llist.printList();
+
+        System.out.print(
+                "\n Lazy Deletion: ");
+
+        llist.Lazy_Deletion(4);
+        llist.Lazy_Deletion(5);
+        llist.Lazy_Deletion(2);
+        llist.Lazy_Deletion(1);
+        llist.Lazy_Deletion(7);
+        llist.Deletion();
+
+        System.out.print(
+                "\n Linked list after deletion");
+        llist.printList();
+    }
+}
