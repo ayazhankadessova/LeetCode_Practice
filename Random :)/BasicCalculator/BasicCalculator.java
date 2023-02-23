5d24711865b695fa5c6d27b43c76a50f4235abe5
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-02-23 23:20:52 +0800

    update: new approach

--- Random :)/BasicCalculator/BasicCalculator.java
+++ Random :)/BasicCalculator/BasicCalculator.java
@@ -1,26 +1,46 @@
-import java.util.*;
 
 class BasicCalculator {
-    public int calculate(String s) {
-        int sum = 0;
-        s = s.trim();
-        int counter = 0;
-        int operator = 1;
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.compare(s.charAt(i), ' ') != 0 && Character.compare(s.charAt(i), '+') != 0 && Character.compare(s.charAt(i), '-') != 0) {
-                int number = s.charAt(i) - '0';
-                sum=sum + (number * operator);
-                if (operator == -1) {
-                    operator =1;
-                }
-            } else {
-                if (Character.compare(s.charAt(i), '-') == 0) {
-                    operator = -1;
-                }
-            }
-        }
 
-        
+    char[] toCalculate;
+    int i = 0;
+
+    public int calculate(String s) {
+        toCalculate = s.toCharArray();
+        return dfs();
+    }
+
+    private int dfs() {
+        int operator = 1;
+        int sum = 0;
+
+        while (i < toCalculate.length)  {
+            if (toCalculate[i] == ')') {
+                break;
+            } else if (toCalculate[i] == '(') {
+                i++;
+                dfs();
+            } else if(toCalculate[i] == '+') {
+                operator = 1;
+            } else if (toCalculate[i] == '-') {
+                operator = -1;
+            } else if (Character.isDigit(toCalculate[i])) { 
+                StringBuilder sb = new StringBuilder();
+                while(i < toCalculate.length && Character.isDigit(toCalculate[i])) {
+                    sb.append(toCalculate[i]);
+                    i++;
+                }
+                i--;
+
+                sum += Integer.parseInt(sb.toString()) * operator;
+            }
+            i++;
+        }
         return sum;
-}
+    }
+
+    public static void main(String[] args) {
+        BasicCalculator basicCalc = new BasicCalculator();
+        System.out.println(basicCalc.calculate("1+(2-3)"));
+    }
+    
 }
