6403f9f420501b9076eafc3c5447e45de423db13
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-02-24 00:04:11 +0800

    change folder name

--- Inspiration/BasicCalculator/BasicCalculator.java
+++ Inspiration/BasicCalculator/BasicCalculator.java
@@ -1,30 +1,34 @@
 
 class BasicCalculator {
 
-// Approach -> DFS:
+    // Approach -> DFS:
 
-// Basic Setup:
-// 1. Create a char array to store operands and operators, everything from string value.
-// 2. Create an index field to keep track of the current index in the array.
-// 3. Call dfs();
+    // Basic Setup:
+    // 1. Create a char array to store operands and operators, everything from
+    // string value.
+    // 2. Create an index field to keep track of the current index in the array.
+    // 3. Call dfs();
 
-// DFS:
-// 1. Iterate through the character array
-// 2. Start with 0 sum and positive operator (no influence)
-// 3. Consider these cases:
-//     Case '(' -> need to find res inside the brackets & add it to res w the operator-> call dfs inside the bracket:
-//         index++;
-//         sum += oper * dfs res
-//     Case ')' -> brackets res ended:
-//         break;
-//     Case '+' / '-' -> set operator to +1/-1;
-//     Case digit -> can have multiple digit numbers, so iterate while char is not digit & add to StringBuilder
-//     * don't forget index-- here bc we stop on smth which isn't a digit, we need to consider it later
+    // DFS:
+    // 1. Iterate through the character array
+    // 2. Start with 0 sum and positive operator (no influence)
+    // 3. Consider these cases:
+    // Case '(' -> need to find res inside the brackets & add it to res w the
+    // operator-> call dfs inside the bracket:
+    // index++;
+    // sum += oper * dfs res
+    // Case ')' -> brackets res ended:
+    // break;
+    // Case '+' / '-' -> set operator to +1/-1;
+    // Case digit -> can have multiple digit numbers, so iterate while char is not
+    // digit & add to StringBuilder
+    // * don't forget index-- here bc we stop on smth which isn't a digit, we need
+    // to consider it later
 
-//     all cases considered:
-//     Add to sum the number we found from StringBuilder * operator
+    // all cases considered:
+    // Add to sum the number we found from StringBuilder * operator
 
-//     done -> iterate
+    // done -> iterate
 
     char[] toCalculate;
     int i = 0;
@@ -39,21 +43,21 @@ class BasicCalculator {
         int sum = 0;
 
         // find res inside the brackets & add it to res w the operator
-        while (i < toCalculate.length)  {
+        while (i < toCalculate.length) {
 
             if (toCalculate[i] == ')') {
                 break;
             } else if (toCalculate[i] == '(') {
                 i++;
-                sum += operator*dfs();
-            } else if(toCalculate[i] == '+') {
+                sum += operator * dfs();
+            } else if (toCalculate[i] == '+') {
                 operator = 1;
             } else if (toCalculate[i] == '-') {
                 operator = -1;
-            } else if (Character.isDigit(toCalculate[i])) { 
+            } else if (Character.isDigit(toCalculate[i])) {
                 // make a number
                 StringBuilder sb = new StringBuilder();
-                while(i < toCalculate.length && Character.isDigit(toCalculate[i])) {
+                while (i < toCalculate.length && Character.isDigit(toCalculate[i])) {
                     sb.append(toCalculate[i++]);
                 }
                 i--;
@@ -79,5 +83,5 @@ class BasicCalculator {
         System.out.println(basicCalc3.calculate("(1+(4+5+2)-3)+(6+8)"));
         // Expected: 23
     }
-    
+
 }
