e633be5c194194d77ebf7c0c64ad31664829b169
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-03-15 17:50:35 +0800

    add: approach

--- DSA_Book/Chapter5/CheckCompletness/Solution.java
+++ DSA_Book/Chapter5/CheckCompletness/Solution.java
@@ -3,6 +3,22 @@ package CheckCompletness;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+ * Approach: BFS - check level by level
+ * 1. Put the root node into the queue
+ * 2. Poll the node from the queue
+ * 3. If the node is null, set the flag to true
+ * 4. If the node is not null, check the flag
+ * 5. If the flag is true, return false
+ * 6. If the flag is false, put the left and right child into the queue
+ * 7. If the queue is empty, return true
+ * 
+ * Time Complexity: O(n)
+ * 
+ * Idea: if we are still iterating the tree and we encounter a null node, then the tree is not complete
+ * 
+ */
+
 // Definition for a binary tree node.
 class TreeNode {
     int val;
